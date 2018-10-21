273b8800b92f0daa240a569a0fe702870af3f1eb
Felipe Guedes <guedes4g@hotmail.com>
2018-10-21 12:23:55 -0300

    IMPORTANTE ajuste de parse para ter NOVO tipo de mensagem...

--- helpers/parse.js
+++ helpers/parse.js
@@ -12,24 +12,24 @@ exports.packet2Data = (data) =>{
 
     const bodyParts = dataParts[1].split(":");
 
-    if (bodyParts.length !== 4)
+    if (bodyParts.length !== 5)
         return null
 
     return {
         errorControl: bodyParts[0],
         apelidoOrigem: bodyParts[1],
         apelidoDestino: bodyParts[2],
-        mensagem: bodyParts[3],
+        tipo: bodyParts[3],
+        mensagem: bodyParts[4],
         rawData: data,
-
         hash: sha1(bodyParts[1] + bodyParts[2] + bodyParts[3])
     }
 }
 
-exports.data2Packet = (data) => {
-    return `2345;${data.errorControl}:${data.apelidoOrigem}:${data.apelidoDestino}:${data.mensagem}`
+exports.data2Packet = (data, code = '2345') => {
+    return `${code};${data.errorControl}:${data.apelidoOrigem}:${data.apelidoDestino}:${data.tipo}:${data.mensagem}`
 }
 
-exports.sendPacketFormat = (from, to, mensagem) => {
-    return `2345;naocopiado:${from}:${to}:${mensagem}`
+exports.sendPacketFormat = (from, to, type , mensagem) => {
+    return `2345;naocopiado:${from}:${to}:${type}:${mensagem}`
 }
