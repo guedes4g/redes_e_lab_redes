5d0848f5da30531a2899859f4b22dfa9e8b001e6
Felipe Guedes <guedes4g@hotmail.com>
2018-10-21 12:24:55 -0300

    Formatando melhor pergunta

--- helpers/cli.js
+++ helpers/cli.js
@@ -1,8 +1,9 @@
 const readline = require('readline');
+const systemout = require('./systemout').format
 var cl = readline.createInterface( process.stdin, process.stdout );
 var question = function(q) {
     return new Promise( (res, rej) => {
-        cl.question( q, (answer) => {
+        cl.question( systemout(q), (answer) => {
             res(answer);
         })
     });
